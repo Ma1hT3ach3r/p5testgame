45d9b8faeb9847702ba6bc8a0530aa8962cdc5b4
MrO <posudar@gmail.com>
2018-04-10 15:23:12 -0400

    Update samplesketch.js

--- samplesketch.js
+++ samplesketch.js
@@ -8,6 +8,8 @@ function draw() {
   background(220);
   fill(mouseX,25,56);
   for(int i=0, i++,i<mouseX||i<mouseY){
-  ellipse(mouseX,mouseY,10,10);
+    while(i<100){
+    ellipse(mouseX+i,mouseY+i,10,10);
+    }
   }
 }
